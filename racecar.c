397590d6ecac45adfe13c32877d4e1383bab57cb
Connor Hannough <12ch33@queensu.ca>
2016-12-07 22:07:10 -0800

    coded serious chunk into program, havent even tried to compile it yet... rip

--- racecar.c
+++ racecar.c
@@ -2,7 +2,7 @@
 #include <stdlib>
 #include <pthread>
 #include <unistd.h>
-
+#include <time.h>
 
 
 //cisc 220 assignment 6
@@ -15,12 +15,42 @@
 
 int car1=0, car2=0, car3=0, car4=0, car5=0
 
-void *ai_cars(void *args) 
+int main() 
 {
+
+return 0;
+}
+
+void *ai_cars(void *ai) 
+{
+	srand ((unsigned)time(NULL));
 	int flag = 0;
 	while (flag == 0) 
 	{
-		}
+	int randy = rand() % 100 + 1;
+	usleep(randy);
+	*ai++;
+	}
 }
 
+void *human_car(void *human)
+{
+	char c = getchar();
+	if (char == '\n')
+	*human++;
+}
 
+void *draw(void *args) //args?
+{
+	//draw start
+	int flag = 0;
+	while (flag == 0) 
+	{
+		// sleep 20 millisec // 5 fps
+		//clear screen
+		//draw
+ 	// if any car == 40
+		//break
+		//print winner
+	}
+}
