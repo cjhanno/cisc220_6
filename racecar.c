21d4cdc62b36b3543c920216da485accacc4e7af
Connor Hannough <12ch33@queensu.ca>
2016-12-07 20:37:52 -0800

    initial code in, need to explore man pages for rand and usleep

---
+++ racecar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib>
+#include <pthread>
+
+//cisc 220 assignment 6
+
+//author: Connor Hannough
+
+//global var
+
+#define NUM_THREADS 6
+
+int car1=0, car2=0, car3=0, car4=0, car5=0
+
+void *ai_cars(void *args) 
+{
+	int flag = 0;
+	while (flag == 0) 
+	{
+		}
+}
+
+
